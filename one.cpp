db1de8cdbefb5c013493ac7a408e43a6931a25f5
Tushar Saxena <stushar345@gmail.com>
2020-09-04 23:23:06 +0530

    Some important points regarding vector functions

--- one.cpp
+++ one.cpp
@@ -1,56 +1,25 @@
 #include<iostream>
+#include<string>
 #include<vector>
-#include<time.h>
-
 using namespace std;
 int main()
-{
-    vector<int > myarray;                        //Declaration
-    myarray.reserve(1000);                          //Reserving the capacity of vector to 1000
-    // myarray.fill(10);                                // fill entire array with same value 10
-    // std::array<int ,5> myarray ={1,2,3,4,5};         //Initialisation
-    int x=1,j;
-    while(x)
-    {
-    cout<<"Enter a value: 1 or 0: ";
-    cin>>x;
-    if (x==0)
-    break;
-    else
-    {
-    cout<<"Enter a value: ";
-    cin>>j;
-    myarray.push_back(j);
-    }
-    }
+{   
+vector<int > Vector;
+Vector.reserve(10);
+Vector.push_back(3);
+Vector.push_back(2);
+Vector.push_back(4);
+Vector.push_back(1);
+Vector.push_back(3);
+Vector.push_back(5);
 
-vector<int>::iterator it;   //pointer used to point to a particular element
 
-for(it=myarray.begin();it!=myarray.end();it++)
-{
-    cout<<*it<<" ";
-
-}
-cout<<"\n";
-
-myarray.pop_back();   //pops out last element from the vector 
-it=myarray.begin();
-it++;
-myarray.erase(it);      //removes element where iterator is pointing
-
-it=myarray.begin();
-myarray.insert(it,9);           //insert a value 9 before the specified position
-
-myarray.erase(++it);                 //remove the element at which iterator is pointing
-myarray.emplace_back(40);               // Add element to the last of vector 
-
-for(it=myarray.begin();it!=myarray.end();it++)
-{
-    cout<<*it<<" ";
+cout<<Vector.front()<<endl;                         //Returns value at beginning
+cout<<*Vector.begin()<<endl;                        //Returns address of first location
+cout<<Vector.back()<<endl;                          //Returns value at last position
+cout<<*(--Vector.end())<<endl;                       //end() function points to the address which is after the last element
+cout<<Vector.size()<<endl;
+cout<<Vector.capacity()<<endl;
 
 }
 
-
-// cout<<myarray.size()<<"\n";              //Actual Size of the vector holding how many items currently 
-// cout<<myarray.capacity();                //limit upto which elements can be inserted
-}
